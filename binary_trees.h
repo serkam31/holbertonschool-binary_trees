0e06d3677b87737ec9988b19891e3668d1c77bf3
Loic2888 <12188@holbertonstudents.com>
2025-12-09 13:34:22 +0100

    feat: function that check if a binary tree is full

--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 
 
